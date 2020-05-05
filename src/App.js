bca691a1857b3bb3c69063231010cda0632fd0af
nailynevarez <nailynevarez@gmail.com>
2020-05-05 00:13:49 -0700

    Add about section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import name from './images/name.png';
 import shape1 from './images/shape.png';
 import shape2 from './images/shape.png';
@@ -13,33 +13,80 @@ import shape10 from './images/shape.png';
 import shape11 from './images/shape.png';
 import shape12 from './images/shape.png';
 import shape13 from './images/shape.png';
+import NailyNevarez from './images/NailyNevarez.png';
 import './App.css';
 
-function App() {
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      homeActive: true,
+      showAbout: false,
+      showText: false,
+    };
+  }
+
+  handleAboutClick = () => {
+    this.setState({
+          showAbout: !this.state.showAbout,
+          showText: !this.state.showText,
+        });
+  }
+
+  handleHomeClick = () => {
+    this.setState({
+          showText: !this.state.showText,
+        });
+
+    setTimeout(() => {
+        this.setState({
+          showAbout: !this.state.showAbout,
+        });
+      }, 300);
+
+  }
+
+render() {
   return (
+    <div className = {this.state.homeActive ? 'fadeIn' : 'fadeOut'}>
+    <div className = {this.state.showAbout ? 'opacityDown' : 'opacityUp'}>
     <div className = "Wrapper">
       <div className = "Logo-Wrapper">
         <div className ="Name-Wrapper">
-          <img src = {name} />
+          <img src = {name} onClick={this.handleAboutClick.bind(this)} />
         </div>
         <div className="Trapezoid-Wrapper">
-          <img src = {shape1} />
-          <img src = {shape2} />
-          <img src = {shape3} />
-          <img src = {shape4} />
-          <img src = {shape5} />
-          <img src = {shape6} />
-          <img src = {shape7} />
-          <img src = {shape8} />
-          <img src = {shape9} />
-          <img src = {shape10} />
-          <img src = {shape11} />
-          <img src = {shape12} />
-          <img src = {shape13} />
+          <a href = "https://www.nailynevarez.com" target="_blank" title = "NAILY NEVAREZ">
+            <img src = {shape1}/>
+          </a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape2} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape3} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape4} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape5} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape6} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape7} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape8} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape9} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape10} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape11} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape12} /></a>
+          <a href = "https://www.nailynevarez.com" target="_blank" ><img src = {shape13} /></a>
         </div>
       </div>
     </div>
+    </div>
+
+    {this.state.showAbout ?
+      <div className = {this.state.showText ? 'fadeInQuick' : 'fadeOutQuick'}>
+      <div className = "About-Wrapper" onClick={this.handleHomeClick.bind(this)}>
+      <p>Carousel is the 2019 - 2020 BFA group at the University of Oregon. With 13 members, our work explores a variety of disciplines and concepts. Click on the sections of our logo to see our individual work.</p>
+      </div>
+      </div>
+
+
+      : null}
+
+    </div>
   );
 }
-
-export default App;
+}
