801dcd2774ca6d9d25dff0a8ce3bc42703f93e84
nailynevarez <nailynevarez@gmail.com>
2020-05-04 20:47:31 -0700

    Create logo layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import name from './images/name.png';
+import shape1 from './images/shape.png';
+import shape2 from './images/shape.png';
+import shape3 from './images/shape.png';
+import shape4 from './images/shape.png';
+import shape5 from './images/shape.png';
+import shape6 from './images/shape.png';
+import shape7 from './images/shape.png';
+import shape8 from './images/shape.png';
+import shape9 from './images/shape.png';
+import shape10 from './images/shape.png';
+import shape11 from './images/shape.png';
+import shape12 from './images/shape.png';
+import shape13 from './images/shape.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to rel.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Wrapper">
+      <div className = "Logo-Wrapper">
+        <div className ="Name-Wrapper">
+          <img src = {name} />
+        </div>
+        <div className="Trapezoid-Wrapper">
+          <img src = {shape1} />
+          <img src = {shape2} />
+          <img src = {shape3} />
+          <img src = {shape4} />
+          <img src = {shape5} />
+          <img src = {shape6} />
+          <img src = {shape7} />
+          <img src = {shape8} />
+          <img src = {shape9} />
+          <img src = {shape10} />
+          <img src = {shape11} />
+          <img src = {shape12} />
+          <img src = {shape13} />
+        </div>
+      </div>
     </div>
   );
 }
