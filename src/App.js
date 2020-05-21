351390a356eddb8a3c1de89065c71e4a41ef4a31
nailynevarez <nailynevarez@gmail.com>
2020-05-20 19:47:08 -0700

    Add cycling names in mobile

--- src/App.js
+++ src/App.js
@@ -36,8 +36,123 @@ export default class App extends Component {
       isHover11: false,
       isHover12: false,
       isHover13: false,
+      isHover1m: false,
+      isHover2m: false,
+      isHover3m: false,
+      isHover4m: false,
+      isHover5m: false,
+      isHover6m: false,
+      isHover7m: false,
+      isHover8m: false,
+      isHover9m: false,
+      isHover10m: false,
+      isHover11m: false,
+      isHover12m: false,
+      isHover13m: false,
     };
   }
+
+
+  componentDidMount(){
+  setTimeout(() => {
+    this.setState({
+      isHover1m: !this.state.isHover1m,
+    });
+    this.loopNames();
+    }, 500);
+}
+
+loopNames = () => {
+  setTimeout(() => {
+      this.setState({
+        isHover1m: !this.state.isHover1m,
+        isHover2m: !this.state.isHover2m,
+      });
+    }, 1500);
+
+    setTimeout(() => {
+        this.setState({
+          isHover2m: !this.state.isHover2m,
+          isHover3m: !this.state.isHover3m,
+        });
+      }, 3000);
+
+      setTimeout(() => {
+          this.setState({
+            isHover3m: !this.state.isHover3m,
+            isHover4m: !this.state.isHover4m,
+          });
+        }, 4500);
+
+      setTimeout(() => {
+          this.setState({
+            isHover4m: !this.state.isHover4m,
+            isHover5m: !this.state.isHover5m,
+          });
+        }, 6000);
+
+      setTimeout(() => {
+          this.setState({
+            isHover5m: !this.state.isHover5m,
+            isHover6m: !this.state.isHover6m,
+          });
+        }, 7500);
+
+      setTimeout(() => {
+          this.setState({
+            isHover6m: !this.state.isHover6m,
+            isHover7m: !this.state.isHover7m,
+          });
+        }, 9000);
+
+      setTimeout(() => {
+          this.setState({
+            isHover7m: !this.state.isHover7m,
+            isHover8m: !this.state.isHover8m,
+          });
+        }, 10500);
+      setTimeout(() => {
+          this.setState({
+            isHover8m: !this.state.isHover8m,
+            isHover9m: !this.state.isHover9m,
+          });
+        }, 12000);
+
+      setTimeout(() => {
+          this.setState({
+            isHover9m: !this.state.isHover9m,
+            isHover10m: !this.state.isHover10m,
+          });
+        }, 13500);
+
+      setTimeout(() => {
+          this.setState({
+            isHover10m: !this.state.isHover10m,
+            isHover11m: !this.state.isHover11m,
+          });
+        }, 15000);
+      setTimeout(() => {
+          this.setState({
+            isHover11m: !this.state.isHover11m,
+            isHover12m: !this.state.isHover12m,
+          });
+        }, 16500);
+
+      setTimeout(() => {
+          this.setState({
+            isHover12m: !this.state.isHover12m,
+            isHover13m: !this.state.isHover13m,
+          });
+        }, 18000);
+
+        setTimeout(() => {
+            this.setState({
+              isHover13m: !this.state.isHover13m,
+              isHover1m: !this.state.isHover1m,
+            });
+            this.loopNames();
+          }, 19500);
+}
 
   handleAboutClick = () => {
     this.setState({
@@ -137,6 +252,21 @@ render() {
       {this.state.isHover11 && <p className = "Name1">JOSH ROLLO</p>}
       {this.state.isHover12 && <p className = "Name1">CULLEN SHARP</p>}
       {this.state.isHover13 && <p className = "Name1">KYLE NELSON</p>}
+      </div>
+      <div className = "Artist-Wrapper-Mobile">
+      {this.state.isHover1m && <p className = "Name1">NAILY NEVAREZ</p>}
+      {this.state.isHover2m && <p className = "Name1">THOMAS NEWLANDS</p>}
+      {this.state.isHover3m && <p className = "Name1">WREN MASON-BLAUG</p>}
+      {this.state.isHover4m && <p className = "Name1">GEORDI HELMICK</p>}
+      {this.state.isHover5m && <p className = "Name1">MARCELLA ROSEN</p>}
+      {this.state.isHover6m && <p className = "Name1">ELIJAH ROTH</p>}
+      {this.state.isHover7m && <p className = "Name1">PHOEBE MALLORY</p>}
+      {this.state.isHover8m && <p className = "Name1">OLIVIA DENTON</p>}
+      {this.state.isHover9m && <p className = "Name1">FELIX NEELEMAN</p>}
+      {this.state.isHover10m && <p className = "Name1">MYA CLOVER-OWENS</p>}
+      {this.state.isHover11m && <p className = "Name1">JOSH ROLLO</p>}
+      {this.state.isHover12m && <p className = "Name1">CULLEN SHARP</p>}
+      {this.state.isHover13m && <p className = "Name1">KYLE NELSON</p>}
       </div>
     </div>
     </div>
